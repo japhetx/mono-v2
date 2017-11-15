04bc3d287dc9bdd8a6627ada3ee2be3d6f28215e
Japhet Mert Obsioma <jpmrobsioma@gmail.com>
2017-11-15 11:37:47 +0800

    Add and Fix Project VIew

--- src/application/signinEstimateController.java
+++ src/application/signinEstimateController.java
@@ -2,6 +2,9 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
@@ -16,17 +19,22 @@ public class signinEstimateController {
 	@FXML
 	private TextField txtPassword;
 
-	public void Signin(ActionEvent event) {
+	public void SigninEstimate(ActionEvent event) throws Exception {
 		if (txtUsername.getText().equals("user")&& txtPassword.getText().equals("pass")) {
+			// Close other window
+			Main.mainWindow.close();
+			welcomeController.signinWindow.close();
 			// New stage here
-			Alert alert = new Alert(AlertType.INFORMATION);
-			Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-			stage.getIcons().add(new Image(this.getClass().getResource("icon.png").toString()));
-			alert.setTitle("Construction Cost Estimate - Sign Success	");
-			alert.setHeaderText("Sign in Success");
-			alert.setContentText("Press OK to continue...");
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/projectsEstimate.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Construction Cost Estimate - Choose project to get started.");
+			primaryStage.getIcons().add(new Image("/application/icon.png"));
+			primaryStage.show();
 
-			alert.showAndWait();
+
 		} else {
 			Alert alert = new Alert(AlertType.ERROR);
 			Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
--- src/application/welcomeController.java
+++ src/application/welcomeController.java
@@ -9,18 +9,18 @@ import javafx.stage.Stage;
 
 public class welcomeController {
 
+	public static Stage signinWindow;
+
 	public void showLoginEstimate(ActionEvent event) throws Exception{
 		Stage primaryStage = new Stage();
+		signinWindow = primaryStage;
 		Parent root = FXMLLoader.load(getClass().getResource("/application/signinEstimate.fxml"));
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("signin.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
-		primaryStage.setTitle("Construction Cost Estimate - Please sign in to continue.");
+		primaryStage.setTitle("Construction Cost Estimate - Create or choose project.");
 		primaryStage.getIcons().add(new Image("/application/icon.png"));
 		primaryStage.show();
-
-
 	}
 
 	public void showLoginPayroll(ActionEvent event ) throws Exception{
